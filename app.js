2e653f8e3f6db82cdf2e1eba1a9df51b8abe4ccb
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2020-12-01 14:18:38 +0530

    feat : added ferb lation translator

---
+++ app.js
@@ -0,0 +1,26 @@
+var txtInput = document.querySelector("#txtInput");
+var txtOutput = document.querySelector("#txtOutput");
+var btnTranslate = document.querySelector("#btnTranslate");
+
+serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTraslationURL(inputText) {
+    return serverURL + "?text=" + inputText;
+}
+
+function errorHandler(error) {
+    console.log("error" , error);
+    alert("There is something wrong with the server, please try again after some time.")
+}
+
+function btnClickHandler() {
+    inputText = txtInput.value;);
+    fetch(getTraslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        txtOutput.innerHTML = json.contents.translated;
+    })
+    .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", btnClickHandler);
